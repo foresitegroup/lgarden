72f4786fe05024f973f382257e4fb4ced1bba0e5
foresitegroup <mark@foresitegrp.com>
2015-02-19 10:03:47 -0600

    Delete dbconfig.php

--- inc/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbHost = "localhost";
-$dbUser = "remediho_lgadmin";
-$dbPass = "Remedi1138";
-$dbName = "remediho_lgarden";
-
-$db = mysql_connect($dbHost, $dbUser, $dbPass);
-mysql_select_db($dbName, $db);
-?>
